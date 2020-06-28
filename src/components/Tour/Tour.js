12ced7066751c5a98a86a0a80f013292c07c95b4
Expedito Andrade <expeditojazz@gmail.com>
2020-06-28 16:49:05 -0300

    add state to city tours

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import "./tour.scss";
 export default class Tour extends Component {
   render() {
     return <h1>Hello from Tour</h1>;
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import Tour from "../Tour";
+import "./tourlist.scss";
+import { tourData } from "../../tourData";
 export default class TourList extends Component {
+  state = {
+    tours: tourData,
+  };
   render() {
     return (
       <div>
